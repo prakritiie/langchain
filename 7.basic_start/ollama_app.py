0eccc5d53bc90c803f6073bda3d1bdd656c77e79
Prakriti <prakritigupta.mailbox@gmail.com>
2026-05-12 00:46:01 +0530

    .

--- 7.basic_start/ollama_app.py
+++ 7.basic_start/ollama_app.py
@@ -1,6 +1,7 @@
+
+
 import os
 from dotenv import load_dotenv
-
 from langchain_community.llms import Ollama
 load_dotenv()
 
@@ -10,7 +11,7 @@ os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACKING"]= "true"
 os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT", "default_project")
 
-import streamlit as st
+
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
@@ -22,17 +23,20 @@ prompt = ChatPromptTemplate.from_messages([
 
 ##streamlit fraemwork
 
+import streamlit as st
+
 st.title("langchain demo with llama 2")
 input_text = st.text_input("ask any question u have in mind :) ")
 
 
 ##ollama llama 2 model
 
-
 llm = Ollama(model="gemma:2b")
 
 output_parser = StrOutputParser()
 chain = prompt | llm | output_parser
+
+
 
 if input_text:
     response = chain.invoke({"question": input_text})
